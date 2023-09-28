c952e76fb2d663202ca0ca8a04e733359c2f2109
Murat Esat Kılıç <muratesat2812@gmail.com>
2023-09-28 17:03:59 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Picker } from 'emoji-mart';
+import 'emoji-mart/css/emoji-mart.css';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [selectedEmoji, setSelectedEmoji] = useState(null);
+
+  const handleEmojiSelect = (emoji) => {
+    setSelectedEmoji(emoji.native);
+    navigator.clipboard.writeText(emoji.native);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Emoji Arama Motoru</h1>
+      <input
+        type="text"
+        placeholder="Emoji ara..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="emoji-container">
+        <Picker
+          style={{ width: '100%' }}
+          emoji=""
+          title="Emoji Seç"
+          onSelect={handleEmojiSelect}
+          theme="auto"
+          search={search}
+        />
+      </div>
+      {selectedEmoji && (
+        <div className="selected-emoji">
+          <p>Seçilen Emoji: {selectedEmoji}</p>
+        </div>
+      )}
     </div>
   );
 }
